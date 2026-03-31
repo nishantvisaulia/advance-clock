cf62b2223d93f7cc332a9b1dd39d238231b22e81
nishantvisaulia <nishantkumar660@gmail.com>
2026-03-31 17:27:44 +0530

    Update clock.js

--- clock.js
+++ clock.js
@@ -1,17 +1,29 @@
 function clockRotation() {
-serInterval(function() {
-var date = new Date();
-var seconds = date.getSeconds();
-var minutes = date.getMinutes();
-var hour = date.getHours();
-var secondsRotation = seconds * 6;
-var minutesRotaion = minutes * 6;
-var hoursRotation = Hours * 30 + minutes / 2;
-$("#seconds").css({
-"-webkit-transform": "rotate(" +
-secondsRotaton + "ge)",
-transform:"rotate(" +
-secondsRotation + "deg)"
-});
-}, 1000);
+  setInterval(function() {
+    var date = new Date();
+
+    var seconds = date.getSeconds();
+    var minutes = date.getMinutes();
+    var hour = date.getHours();
+
+    var secondsRotation = seconds * 6;
+    var minutesRotation = minutes * 6;
+    var hoursRotation = hour * 30 + minutes / 2;
+
+    $("#seconds").css({
+      "-webkit-transform": "rotate(" + secondsRotation + "deg)",
+      "transform": "rotate(" + secondsRotation + "deg)"
+    });
+
+    $("#minutes").css({
+      "-webkit-transform": "rotate(" + minutesRotation + "deg)",
+      "transform": "rotate(" + minutesRotation + "deg)"
+    });
+
+    $("#hours").css({
+      "-webkit-transform": "rotate(" + hoursRotation + "deg)",
+      "transform": "rotate(" + hoursRotation + "deg)"
+    });
+
+  }, 1000);
 }
